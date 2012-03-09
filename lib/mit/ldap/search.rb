38060766008c4611d17f2c328d691b4cf427d4f7
Eduardo Gutierrez <edd_d@mit.edu>
2012-03-09 02:45:50 -0500

    Make tests and module conditional on if on campus
    Problem with working with MIT ldap server is that in order to not
    require authentication, it limits access to the MIT network. Thus it's
    hard to develop an application that depends MIT::LDAP::Search as a
    network connection is required to connect via VPN.  If not on campus,
    the Ldaptic :search and :find methods will return empty stubs of what
    they would normally return.

--- lib/mit/ldap/search.rb
+++ lib/mit/ldap/search.rb
@@ -4,11 +4,24 @@ require 'ldaptic'
 module MIT
   module LDAP
     module Search
-      include Ldaptic::Module(
-        host: 'ldap-too.mit.edu',
-        base: 'dc=mit,dc=edu',
-        adapter: :ldap_conn
-      )
+      if MIT.on_campus_network?
+        include Ldaptic::Module(
+          host: 'ldap-too.mit.edu',
+          base: 'dc=mit,dc=edu',
+          adapter: :ldap_conn
+        )
+      else
+        class InetOrgPerson
+        end
+
+        def self.search(*args)
+          []
+        end
+
+        def self.find(*args)
+          InetOrgPerson.new
+        end
+      end
     end
   end
 end
--- spec/lib/ldap/search_spec.rb
+++ spec/lib/ldap/search_spec.rb
@@ -3,9 +3,16 @@ require 'mit-ldap'
 module MIT
   module LDAP
     describe Search do
-      it "includes Ldaptic::Module" do
-        should respond_to :search
-        should respond_to :find
+      if MIT.on_campus_network?
+        it "includes Ldaptic::Module" do
+          should respond_to :search
+          should respond_to :find
+        end
+      else
+        it "defines stubs that return empty arrays" do
+          Search.search.should eq []
+          Search.find.should be_instance_of Search::InetOrgPerson
+        end
       end
     end
   end
