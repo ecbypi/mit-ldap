366b908c4032a889d7e588eb302749bde5c5497f
Eduardo Gutierrez <edd_d@mit.edu>
2012-11-05 11:40:22 -0500

    Decrease indentation

--- lib/mit/ldap.rb
+++ lib/mit/ldap.rb
@@ -1,64 +1,61 @@
 require 'logger'
 
-module MIT
-  module LDAP
+module MIT::LDAP
+  class << self
+    attr_accessor :logger
 
-    class << self
-      attr_accessor :logger
+    def logger
+      @logger ||= Logger.new('mit-ldap.log')
 
-      def logger
-        @logger ||= Logger.new('mit-ldap.log')
+      adapter_present? ? adapter.logger : @logger
+    end
 
-        adapter_present? ? adapter.logger : @logger
-      end
+    def search(options = {})
+      []
+    end
 
-      def search(options = {})
-        []
-      end
+    def connect!
+      if MIT.on_campus? && !adapter_present?
+        include Ldaptic::Module(
+          :adapter => :ldap_conn,
+          :connection => ::LDAP::Conn.new('ldap-too.mit.edu'),
+          :host => 'ldap-too.mit.edu',
+          :base => 'dc=mit,dc=edu',
+          :logger => logger
+        )
 
-      def connect!
-        if MIT.on_campus? && !adapter_present?
-          include Ldaptic::Module(
-            :adapter => :ldap_conn,
-            :connection => ::LDAP::Conn.new('ldap-too.mit.edu'),
-            :host => 'ldap-too.mit.edu',
-            :base => 'dc=mit,dc=edu',
-            :logger => logger
-          )
-
-          instance_eval do
-            def search(options = {})
-              connected? || reconnect! ? super : []
-            end
+        instance_eval do
+          def search(options = {})
+            connected? || reconnect! ? super : []
           end
-        elsif MIT.on_campus? && adapter_present?
-          reconnect!
         end
-
-        connected?
+      elsif MIT.on_campus? && adapter_present?
+        reconnect!
       end
 
-      def connected?
-        !connection.nil?
-      end
+      connected?
+    end
 
-      def reconnect!
-        if adapter_present?
-          adapter.instance_variable_set(:@connection, ::LDAP::Conn.new('ldap-too.mit.edu'))
-        elsif !adapter_present?
-          connect!
-        end
-      end
+    def connected?
+      !connection.nil?
+    end
 
-      private
-
-      def connection
-        adapter.instance_variable_get(:@connection) if adapter_present?
+    def reconnect!
+      if adapter_present?
+        adapter.instance_variable_set(:@connection, ::LDAP::Conn.new('ldap-too.mit.edu'))
+      elsif !adapter_present?
+        connect!
       end
+    end
 
-      def adapter_present?
-        respond_to?(:adapter)
-      end
+    private
+
+    def connection
+      adapter.instance_variable_get(:@connection) if adapter_present?
+    end
+
+    def adapter_present?
+      respond_to?(:adapter)
     end
   end
 end
